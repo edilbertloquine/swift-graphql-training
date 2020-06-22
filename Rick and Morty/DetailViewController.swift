4d90b515a48481fc1606a72a69549e6353c70c78
Edilbert Loquine <edilbert@whitewidget.com>
2020-06-22 21:36:15 +0800

    Initial Commit

---
+++ Rick and Morty/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  Rick and Morty
+//
+//  Created by Edilbert Loquine on 6/22/20.
+//  Copyright © 2020 Edilbert Loquine. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        configureView()
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
